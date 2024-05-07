2509b76b0b2395c469fdaf86b8e9863963341f14
Denchik918 <volnata121230@mail.ru>
2024-05-07 19:48:53 +0300

    первая функция с нахождением нужных чисел

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 string[] str2 = new string[str1.Length];
 
 void SelectionOfWords(string[] str1, string[] str2)
+{
+    int cnt = 0;
+    for (int i = 0; i < str1.Length; i++)
+    {
+        if (str1[i].Length <= 3)
+        {
+            str2[cnt] = str1[i];
+            cnt++;
+        }
+    }
+}
